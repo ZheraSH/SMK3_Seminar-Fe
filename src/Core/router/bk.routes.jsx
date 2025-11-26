22729881f78304692cc32200d20f4f64b90e2b02
Syaiful-i <sayiful551@gmail.com>
2025-11-26 08:57:16 +0700

    fix: root BK

--- src/Core/router/bk.routes.jsx
+++ src/Core/router/bk.routes.jsx
@@ -1,8 +1,11 @@
 import MainMonitoringAbsen from "../../view/pages/BK/monitoring Absen/MainAttendanceMonitoring";
+import VerifikasiIzin from "../../view/pages/BK/verifikasi/Verifikasi-izin";
+// import StatistikGlobal from "../../view/pages/BK/statistikglobal/MainStatistikGlobal";
 import MainDashboard from "../../view/components/elements/MainDashboard";
 import BodyDashboard from "../../view/pages/BK/home/BodyDasboardBk";
 import { LayouthBK } from "../../view/layouts/BkLayouth";
 import ProtectedRoute from "./ProtectedRoute";
+
 export const BkRoutes = [
     {
         path: "/bk-home",
@@ -14,8 +17,8 @@ export const BkRoutes = [
                     { index: true, element: <BodyDashboard /> },
                     { path: "dashboard", element: <MainDashboard/> },
                     {path: "monitoring-absen", element: <MainMonitoringAbsen />},
-                    // {path : "statistik-global", element: <MainStatistikGlobal/>},
-                    // {path: "verifikasi-izin", element: <MainVerifikasiIzin/>}
+                    // {path : "statistik-global", element: <StatistikGlobal/>},
+                    {path: "verifikasi-izin", element: <VerifikasiIzin/>}
                 ]
             }
            
