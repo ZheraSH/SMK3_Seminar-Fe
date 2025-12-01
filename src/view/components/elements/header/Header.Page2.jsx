7f9e69281ab96fcaf5846ab1fe45014b4bffa81b
nando <ham280207@gmail.com>
2025-12-01 10:41:10 +0700

    feat: Header2 Reuseable

--- src/view/components/elements/header/Header.Page2.jsx
+++ src/view/components/elements/header/Header.Page2.jsx
@@ -1,34 +1,33 @@
 import { Calendar1, UserRoundCheck, UsersRound } from "lucide-react";
 
 const HeaderPage2 = (props) => {
-    const { h1, p } = props;
   
-    return (
-      <div className="relative w-full h-[156px] mt-6 bg-[url('/images/background/bg04.png')] bg-right bg-cover bg-no-repeat rounded-[15px] mb-10 shadow-md">
-        <div className="absolute inset-0 flex flex-col mt-2 rounded-[6px]">
-          <div className="ml-6">
-            <h1 className="text-white text-[30px] font-semibold drop-shadow-lg">
-              {h1}
-            </h1>
-            <p className="text-white text-[14px] font-light drop-shadow-md">
-              {p}
-            </p>
-            <div className="flex mt-10 gap-2 text-[14px] text-white">
-                
-                <UserRoundCheck className="w-[20px] h-[20px] "/>
-                <p>Valen Ibrahim </p>
+  const { h1, p, user, count_student, schoolyears  } = props;
 
-                <UsersRound className="w-[20px] h-[20px] ml-3"/>
-                <p>34</p>
+  return (
+    <div className="relative w-full h-[156px] mt-6 bg-[url('/images/background/bg04.png')] bg-right bg-cover bg-no-repeat rounded-[15px] mb-10 shadow-md">
+      <div className="absolute inset-0 flex flex-col mt-2 rounded-[6px]">
+        <div className="ml-6">
+          <h1 className="text-white text-[30px] font-semibold drop-shadow-lg">
+            {h1}
+          </h1>
+          <p className="text-white text-[14px] font-light drop-shadow-md">
+            {p}
+          </p>
+          <div className="flex mt-10 gap-2 text-[14px] text-white">
+            <UserRoundCheck className="w-[20px] h-[20px] " />
+            {user}
 
-                <Calendar1 className="w-[20px] h-[20px] ml-3"/>
-                <p>2025/2026</p>
-            </div>
+            <UsersRound className="w-[20px] h-[20px] ml-3" />
+            <p>{count_student}</p>
+
+            <Calendar1 className="w-[20px] h-[20px] ml-3" />
+            <p>{schoolyears}</p>
           </div>
         </div>
       </div>
-    );
-  };
-  
-  export default HeaderPage2;
-  
+    </div>
+  );
+};
+
+export default HeaderPage2;
