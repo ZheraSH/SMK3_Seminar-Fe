7390dc21ea323a616508dea228b44d871d4b7505
nando <ham280207@gmail.com>
2025-12-01 11:25:22 +0700

    feat: AdminRoute Maple

--- src/core/router/admin.routes.jsx
+++ src/core/router/admin.routes.jsx
@@ -4,7 +4,6 @@ import Login from "../../view/components/elements/login/MainLogin";
 import NotFound from "../../view/pages/NotFound";
 import { MainStudent } from "../../view/pages/operator/student/MainStudent";
 import BodyDashboard from "../../view/pages/operator/home/BodyDashboard";
-import MainMaple from "../../view/pages/operator/subjects/MainSubjects";
 import MainClass from "../../view/pages/operator/class-major/MainClass";
 import MainMajor from "../../view/pages/operator/class-major/MainMajor";
 import { RfidManagement } from "../../view/pages/operator/rfid/MainRfid";
@@ -14,29 +13,33 @@ import AbsenRfid from "../../view/pages/operator/absenrfid/AbsenRfid";
 import ClassStudents from "../../view/pages/operator/class-major/class-students/MainClassStudent";
 import Shcedule from "../../view/pages/operator/schedule-clock/MainLayout";
 import ProtectedRoute from "./ProtectedRoute";
+// import TahunAjaran from "../../view/pages/operator/schoolyears/MainSchoolYears";
+import MainMaple from "../../view/pages/operator/subjects/MainSubjects";
 
 export const AdminRoutes = [
   {
     path: "/home",
-    element:(<ProtectedRoute allowedRoles={["school_operator"]} />),
-    children: [{
-      element : <DashboardLayouth />,
-      children : [
-        { index: true, element: <BodyDashboard /> },
-        { path: "dashboard", element: <MainDashboard /> },
-        { path: "jadwalpelajaran", element: <ClassScheduleManager/>},
-        { path: "guru", element: <TeacherMain/> },
-        { path: "Major", element: <MainMajor /> },
-        { path: "class", element: <MainClass /> },
-        { path: "classStudents/:id", element: <ClassStudents /> },
-        { path: "maple", element: <MainMaple /> },
-        { path: "siswa", element: <MainStudent /> },
-        { path : "absen-rfid", element: <AbsenRfid />},
-        { path : "rfid", element: <RfidManagement />},
-        { path : "Shedule", element: <Shcedule />},
-      ]
-
-    }],
+    element: <ProtectedRoute allowedRoles={["school_operator"]} />,
+    children: [
+      {
+        element: <DashboardLayouth />,
+        children: [
+          { index: true, element: <BodyDashboard /> },
+          { path: "dashboard", element: <MainDashboard /> },
+          { path: "jadwalpelajaran", element: <ClassScheduleManager /> },
+          { path: "guru", element: <TeacherMain /> },
+          { path: "Major", element: <MainMajor /> },
+          { path: "class", element: <MainClass /> },
+          { path: "classStudents/:id", element: <ClassStudents /> },
+          { path: "maple", element: <MainMaple /> },
+          // { path: "tahun-ajaran", element: <TahunAjaran /> },
+          { path: "siswa", element: <MainStudent /> },
+          { path: "absen-rfid", element: <AbsenRfid /> },
+          { path: "rfid", element: <RfidManagement /> },
+          { path: "Shedule", element: <Shcedule /> },
+        ],
+      },
+    ],
   },
   {
     path: "/",
