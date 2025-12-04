3c6bffcc0177e1796448a2913237dd8104df0c68
Dwi Rahmawati <xqsdecii@gmail.com>
2025-12-04 09:21:02 +0700

    docs: moving file to userDashboard

---
+++ src/Core/api/role-student/dashboard/useDashboard.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export const getDashboardStudent = async  () => {
+    const token = localStorage.getItem("token");
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    console.log("TOKEN:", token);
+    console.log("USER DATA:", userData);
+
+    const roles = userData?.roles || [];
+        if (!userData || !roles.includes("student")) {
+            console.warn("User bukan student atau data user tidak ditemukan.");
+            return null;
+    }
+
+    try {
+        const res = await axios.get (
+            `http://127.0.0.1:8000/api/student/dashboard`,
+            {
+                headers : {
+                    Authorization : token ? `Bearer ${token}` : "",
+                    Accept : "application/json",
+
+                },
+            }
+        );
+        return res.data;
+    }
+    catch (error) {
+        console.log("Gagal mengambil jadwal: ", error);
+        return null;
+    }
+}
--- src/view/pages/student/home/BodyDashboardStuent.jsx
+++ src/view/pages/student/home/BodyDashboardStuent.jsx
@@ -1,32 +1,40 @@
-import { useEffect,useState } from "react";
-import { presence,schedule,status,history } from "../../../../Core/Data/SiswaData";
 import ProfileHeader from "./components/ProfileHeader";
 import Card from "./components/AttendanceCards";
 import TableSchedule from "./components/TableSchedule";
 import TableHistory from "./components/TableHistory";
+import { DashboardStudent } from "../../../../Core/hooks/role-student/dashboard/DashboardStudent";
 
-export default function BodyDashboard () {
-    const [user, setUser] = useState({ name: "", email: "" });
+export default function BodyDashboard() {
 
-    useEffect(() => {
-        const storedUser = localStorage.getItem("userData");
-        if (storedUser) setUser(JSON.parse(storedUser));
-      }, []);
-      
+    const {
+        profil,
+        attendance,
+        schedule,
+        error,
+        loading,
+        permissions
+
+    } = DashboardStudent ();
+
     return (
         <div className="justify-center mx-3 mb-10 mt-5">
-           <ProfileHeader user={user} />
-            <Card presence={presence} />
+            <ProfileHeader user={profil} />
+            <Card  attendance={attendance} />
             <div className="flex flex-col md:flex-row lg:flex-row gap-5 w-full">
-                <div className="flex flex-col gap-5 w-full md:w-[535px] lg:w-[675px] min-h-[300px] rounded-xl shadow-lg border border-gray-300 p-5">
+                <div className="flex flex-col gap-5 w-full md:w-[535px] lg:w-[675px] lg:h-[383px] overflow-auto rounded-xl shadow-lg border border-gray-300 p-5">
                     <h1 className="text-[18px] md:text-[24px] font-semibold">Jadwal Pelajaran Hari Ini</h1>
-                    <TableSchedule schedule={schedule} />
+                    <TableSchedule 
+                    schedule={schedule}
+                    error={error}
+                    loading={loading}
+                     />
                 </div>
-                <div className="w-full md:w-[404px] lg:w-[434px] min-h-[300px] gap-5 rounded-xl shadow-lg border border-gray-300 p-5 flex flex-col">
+                <div className="w-full md:w-[404px] lg:w-[434px] h-[357px] gap-5 rounded-xl shadow-lg border border-gray-300 p-5 flex flex-col">
                     <h1 className="text-[18px] md:text-[24px] font-semibold">Riwayat Izin Terbaru</h1>
                     <TableHistory 
-                        status={status}
-                        history={history}
+                        history={permissions}
+                        error={error}
+                        loading={loading}
                     />
                 </div>
             </div>
