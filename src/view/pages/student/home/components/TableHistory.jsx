064ee51378ca287e96bb3f4ea303b5e354052bf0
Dwi Rahmawati <xqsdecii@gmail.com>
2025-12-04 09:25:02 +0700

    feat: information error,loading

--- src/view/pages/student/home/components/TableHistory.jsx
+++ src/view/pages/student/home/components/TableHistory.jsx
@@ -1,4 +1,45 @@
-export default function TableHistory ({history,status}) {
+
+export default function TableHistory ({history,error,loading}) {
+    const style = [
+      {
+          id : "Disetujui",
+          style: " bg-[#10B981]/20 text-[#10B981]"
+      },
+      {
+          id:"Ditolak",
+          style: "bg-[#FF5E53]/20 text-[#FF5E53]"
+      },
+      {
+          id:"Menunggu",
+          style: " bg-[#FACC15]/20 text-[#FACC15]"
+      },
+    ]
+
+    if (loading) {
+        return (
+            <div className="w-full h-[200px] flex justify-center items-center">
+                <p className="text-gray-500 text-sm animate-pulse">Loading data...</p>
+            </div>
+        );
+    }
+
+   
+    if (error) {
+        return (
+            <div className="w-full h-[200px] flex justify-center items-center">
+                <p className="text-red-500 text-sm">{error}</p>
+            </div>
+        );
+    }
+
+    if (!history || history.length === 0) {
+        return (
+            <div className="w-full min-h-[200px] flex items-center justify-center text-gray-500 text-sm">
+                Tidak ada data izin Terbaru.
+            </div>
+        );
+    }
+
     return (
         <table className="w-full table-auto border-collapse min-h-[200px] md:h-[261px]">
             <thead>
@@ -10,14 +51,14 @@ export default function TableHistory ({history,status}) {
             </thead>
             <tbody>
                 {history.map((h,index) => {
-                    const matchStatus = status.find((s) => s.id === h.status);
+                    const matchStatus = style.find((s) => s.id === h.Status);
                     return (
                         <tr key={index} className="border border-gray-300 h-[50px] md:h-[57px]  text-[10px] md:text-[12px]  lg:text-[14px]">
-                            <td className="pl-3  font-medium">{h.date}</td>
-                            <td className=" text-center font-medium">{h.reason}</td>
+                            <td className="pl-3  font-medium">{h.Tanggal}</td>
+                            <td className=" text-center font-medium">{h.Alasan}</td>
                             <td className="text-center">
                                 <span className={`px-2 md:px-3 py-0.5 rounded-md inline-flex justify-center items-center font-medium ${matchStatus?.style}`}>
-                                    {h.status}
+                                    {h.Status}
                                 </span>
                             </td>
                         </tr>
--- src/view/pages/student/home/components/TableSchedule.jsx
+++ src/view/pages/student/home/components/TableSchedule.jsx
@@ -1,4 +1,28 @@
-export default function TableSchedule ({schedule}){
+export default function TableSchedule ({schedule,loading,error}){
+
+    if (loading) {
+        return (
+            <div className="w-full h-[250px] flex justify-center items-center">
+                <p className="text-gray-500 text-sm animate-pulse">Loading jadwal...</p>
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div className="w-full h-[250px] flex justify-center items-center">
+                <p className="text-red-500 text-sm">{error}</p>
+            </div>
+        );
+    }
+
+    if (!schedule || schedule.length === 0) {
+        return (
+            <div className="w-full h-[250px] flex justify-center items-center">
+                <p className="text-gray-500 text-sm">Tidak ada jadwal hari ini.</p>
+            </div>
+        );
+    }
     return (
         <table className="w-full table-auto border-collapse min-h-[250px] md:h-[283px]">
             <thead>
@@ -6,16 +30,16 @@ export default function TableSchedule ({schedule}){
                     <th className="rounded-tl-md text-left font-medium pl-3 py-2 w-[30%]">Mapel</th>
                     <th className="font-medium text-left w-[25%]">Guru</th>
                     <th className="font-medium text-left w-[20%]">Jam</th>
-                    <th className="rounded-tr-md font-medium text-left w-[25%]">Kelas</th>
+                    <th className="rounded-tr-md font-medium text-center w-[25%]">Kelas</th>
                 </tr>
             </thead>
             <tbody>
                 {schedule.map((s,index) => (
                     <tr key={index} className="border border-gray-300  text-[10px] md:text-[12px]  lg:text-[14px]">
-                        <td className="pl-3 py-2 font-medium">{s.mapel}</td>
-                        <td className="font-medium">{s.teacher}</td>
-                        <td className=" font-medium">Jam Ke {s.time}</td>
-                        <td className=" font-medium">{s.class}</td>
+                        <td className="pl-3 py-2 font-medium">{s.mata_pelajaran}</td>
+                        <td className="font-medium">{s.guru}</td>
+                        <td className=" font-medium">{s.penempatan}</td>
+                        <td className=" font-medium text-center">{s.classroom || "-"}</td>
                     </tr>
                 ))}
             </tbody>
