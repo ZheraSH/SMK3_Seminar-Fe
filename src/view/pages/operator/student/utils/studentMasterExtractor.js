5006ca9b1af22c443f7008d92a9226cead3dbbe9
nando <ham280207@gmail.com>
2025-12-18 09:14:05 +0700

    fix: filter droopdown major and levelclass

--- src/view/pages/operator/student/utils/studentMasterExtractor.js
+++ src/view/pages/operator/student/utils/studentMasterExtractor.js
@@ -2,6 +2,8 @@ export const extractStudentMasters = (students) => {
   const genders = new Map();
   const classrooms = new Map();
   const schoolYears = new Map();
+  const majors = new Map();
+  const levelClasses = new Map();
 
   students.forEach((student) => {
     if (student.gender?.value && student.gender?.label) {
@@ -18,14 +20,21 @@ export const extractStudentMasters = (students) => {
         student.classroom.school_year
       );
     }
+
+    if (student.classroom?.major) {
+      majors.set(student.classroom.major, student.classroom.major);
+    }
+
+    if (student.classroom?.level_class) {
+      levelClasses.set(student.classroom.level_class, student.classroom.level_class);
+    }
   });
 
   return {
     genders: Array.from(genders, ([value, label]) => ({ value, label })),
     classrooms: Array.from(classrooms, ([value, label]) => ({ value, label })),
-    schoolYears: Array.from(schoolYears, ([value, label]) => ({
-      value,
-      label,
-    })),
+    schoolYears: Array.from(schoolYears, ([value, label]) => ({ value, label })),
+    majors: Array.from(majors, ([value, label]) => ({ value, label })),
+    levelClasses: Array.from(levelClasses, ([value, label]) => ({ value, label })),
   };
 };
