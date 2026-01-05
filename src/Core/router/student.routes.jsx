63feb5616d8861fe9e22dc90a178b14d25acef12
Dwi Rahmawati <xqsdecii@gmail.com>
2026-01-05 09:09:04 +0700

    Refactor: Route Student

--- src/Core/router/student.routes.jsx
+++ src/Core/router/student.routes.jsx
@@ -2,9 +2,9 @@ import MainClassStudent from "../../view/pages/student/Class/MainClass";
 import { LayouthSiswa } from "../../view/layouts/SiswaLayouth";
 import MainScheduleStudent from "../../view/pages/student/schedule/MainSchedule";
 import PermissionManagement from "../../view/pages/student/permission-management/PermissionManagement";
+import BodyDashboard from "../../view/pages/student/home/BodyDashboardStuent";
 import ProtectedRoute from "./ProtectedRoute";
 import MainDashboard from "../../view/components/elements/MainDashboard";
-import BodyDashboard from "../../view/pages/operator/home/BodyDashboard";
 import AbsentStudentMain from "../../view/pages/student/absence/AbsenceStudent";
 
 
@@ -21,7 +21,7 @@ export const StudentRoutes =[
                 { index: true, element: < BodyDashboard/>  },
                 { path: "dashboard", element: <MainDashboard/> },
                 { path: "student-class", element: <MainClassStudent /> },
-                { path: "absen-student", element: <AbsentStudentMain /> },
+                { path: "student-attendance", element: <AbsentStudentMain /> },
                 { path: "student-schedule", element: <MainScheduleStudent  /> },
                 { path: "student-license", element: <PermissionManagement/> },
               ]
