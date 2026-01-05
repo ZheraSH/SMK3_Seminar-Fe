d9260973f35193c2b4d7507f81f9e3c6ece3d2c1
afantino02042007-dot <afantino02042007@gmail.com>
2026-01-05 09:00:13 +0700

    refactor: route student

--- src/Core/router/student.routes.jsx
+++ src/Core/router/student.routes.jsx
@@ -4,7 +4,10 @@ import MainScheduleStudent from "../../view/pages/student/schedule/MainSchedule"
 import PermissionManagement from "../../view/pages/student/permission-management/PermissionManagement";
 import ProtectedRoute from "./ProtectedRoute";
 import MainDashboard from "../../view/components/elements/MainDashboard";
-import BodyDashboard from "../../view/pages/student/home/BodyDashboardStuent";
+import BodyDashboard from "../../view/pages/operator/home/BodyDashboard";
+import AbsentStudentMain from "../../view/pages/student/absence/AbsenceStudent";
+
+
 
 export const StudentRoutes =[
    
@@ -18,7 +21,7 @@ export const StudentRoutes =[
                 { index: true, element: < BodyDashboard/>  },
                 { path: "dashboard", element: <MainDashboard/> },
                 { path: "student-class", element: <MainClassStudent /> },
-                // { path: "absen-student", element: <AbsentStudentMain /> },
+                { path: "absen-student", element: <AbsentStudentMain /> },
                 { path: "student-schedule", element: <MainScheduleStudent  /> },
                 { path: "student-license", element: <PermissionManagement/> },
               ]
