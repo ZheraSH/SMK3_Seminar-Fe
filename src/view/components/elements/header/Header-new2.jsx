dc4952e0c27d70e21b31bfc33e589a025e7e826d
Syaiful-i <sayiful551@gmail.com>
2026-01-12 07:42:08 +0700

    Refactor: Header

--- src/view/components/elements/header/Header-new2.jsx
+++ src/view/components/elements/header/Header-new2.jsx
@@ -1,11 +1,11 @@
 import {  Search, Users, UserCheck2, Calendar, GraduationCap, RefreshCw, Plus, ArrowLeft, X  } from 'lucide-react'; 
 function Header2 ({title = "" , teacher = "" , studentCont= "", academicYear = "" , src= ""}) {
   return(
-    <div className='container mx-auto'>
+    <div className='w-full'>
       <div className='w-full h-[130px] bg-[#BFDBFE] rounded-[16px] flex justify-between'>
-        <div className=' pt-4 pl-6'>
-          <span className='lg:text-[24px] md:text-[23px] text-[16px]  font-semibold'>Kelas - {title}</span>
-          <div className=" flex flex-col items-start space-y-1 mt-1 md:flex-row md:items-center md:space-y-0 md:space-x-4 md:mt-8 lg:space-x-8">
+        <div className=' lg:pt-4 md:pt-4 pt-6  pl-6'>
+          <span className='lg:text-[24px] md:text-[23px] text-[16px] font-semibold'>{title}</span>
+          <div className=" flex items-start space-x-3 mt-5 flex-row md:items-center md:space-y-0 md:space-x-4 md:mt-8 lg:space-x-8">
               <div className="flex items-center space-x-2">
                   <UserCheck2 size={20}/>
                   <span className='lg:text-[14px] md:text-[13px] text-[12px] text-center'>{teacher}</span>
@@ -21,7 +21,7 @@ function Header2 ({title = "" , teacher = "" , studentCont= "", academicYear = "
           </div>
         </div>
         <div className='lg:mt-2 md:mt-2 mt-4'>
-          <img src={src} alt="" className='lg:w-[227px] lg:h-[130px] md:w-[227px] md:h-[130px] w-[160px] h-[110px]'/>
+          <img src={src} alt="" className='lg:w-[227px] lg:h-[130px] md:w-[227px] md:h-[130px] w-[160px] h-[110px] lg:flex hidden'/>
         </div>
       </div>
     </div>
