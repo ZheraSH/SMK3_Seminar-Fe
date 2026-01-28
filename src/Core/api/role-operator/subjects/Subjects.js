78d6e6d51e83bf0cfaaba3729606ebd094e7126d
Dwi Rahmawati <xqsdecii@gmail.com>
2026-01-28 22:43:09 +0700

    Fix: Api Token Mapel

--- src/Core/api/role-operator/subjects/Subjects.js
+++ src/Core/api/role-operator/subjects/Subjects.js
@@ -1,13 +1,19 @@
 import axios from "axios"
 import { notify } from "../../../hooks/notification/notify"
 
-
 const API_BASE_URL = "http://127.0.0.1:8000/api"
 
 export const getSubjects = async (page = 1, search = "") => {
+  const token = localStorage.getItem("token");
+
   try {
-    const res = await axios.get(`${API_BASE_URL}/subjects?page=${page}&search=${search}`)
-    return res.data 
+    const res = await axios.get(`${API_BASE_URL}/subjects?page=${page}&search=${search}`, {
+      headers: {
+        Authorization: token ? `Bearer ${token}` : `Bearer `,
+        Accept: "application/json",
+      },
+    })
+    return res.data
   } catch (err) {
     console.error("Gagal ambil data subjects:", err)
     throw err
@@ -15,20 +21,33 @@ export const getSubjects = async (page = 1, search = "") => {
 }
 
 export const addSubject = async (subject) => {
+  const token = localStorage.getItem("token");
+
   try {
-    const response = await axios.post(`${API_BASE_URL}/subjects`, subject);
+    const response = await axios.post(`${API_BASE_URL}/subjects`, subject, {
+      headers: {
+        Authorization: token ? `Bearer ${token}` : `Bearer `,
+        Accept: "application/json",
+      },
+    });
     notify("Data Berhasil Ditambah");
     return response.data;
   } catch (err) {
-    throw err; // lempar ke komponen
+    throw err;
   }
 };
 
-
 export const updateSubject = async (id, name) => {
+  const token = localStorage.getItem("token");
+
   try {
     const response = await axios.put(`${API_BASE_URL}/subjects/${id}`, {
       name: name,
+    }, {
+      headers: {
+        Authorization: token ? `Bearer ${token}` : `Bearer `,
+        Accept: "application/json",
+      },
     })
     notify("Data Berhasil Diperbarui")
     return response.data
@@ -39,8 +58,15 @@ export const updateSubject = async (id, name) => {
 }
 
 export const deleteSubject = async (id) => {
+  const token = localStorage.getItem("token");
+
   try {
-    const response = await axios.delete(`${API_BASE_URL}/subjects/${id}`)
+    const response = await axios.delete(`${API_BASE_URL}/subjects/${id}`, {
+      headers: {
+        Authorization: token ? `Bearer ${token}` : `Bearer `,
+        Accept: "application/json",
+      },
+    })
     console.log(` Subject ${id} berhasil di hapus`)
     notify("Data Berhasil Dihapus")
     return response.data
