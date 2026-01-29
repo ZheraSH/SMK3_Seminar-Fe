fc77bc517923c0b90e8255d8e251eb897533f2a0
nando <ham280207@gmail.com>
2026-01-30 02:54:48 +0700

    refactor: typo notif schoolyears

--- src/Core/api/role-operator/schoolYear/Schoolyear.jsx
+++ src/Core/api/role-operator/schoolYear/Schoolyear.jsx
@@ -5,12 +5,12 @@ import { notify } from "../../../hooks/notification/notify"
 
 const API_BASE_URL = "http://127.0.0.1:8000/api"
 
-export const getSchoolYears= async (page = 1,search = "") => {
+export const getSchoolYears = async (page = 1, search = "") => {
   try {
-    const res = await api.get(`/school-years`,{
-        params: { page: page, search: search }
+    const res = await api.get(`/school-years`, {
+      params: { page: page, search: search }
     })
-    return res.data 
+    return res.data
   } catch (err) {
     console.error("Gagal ambil data subjects:", err)
     throw err
@@ -18,35 +18,35 @@ export const getSchoolYears= async (page = 1,search = "") => {
 }
 
 export const createSchoolYears = async (formData) => {
-    try {
-        const res = await api.post(`/school-years`, formData)
-        notify("Data Berhasil Ditambah");
-        return res.data; 
-    } catch (err) {
-        console.error("Gagal menambah school year:", err.response ? err.response.data : err)
-        throw err.response ? err.response.data : err;
-    }
+  try {
+    const res = await api.post(`/school-years`, formData)
+    notify("Data Berhasil Ditambah");
+    return res.data;
+  } catch (err) {
+    console.error("Gagal menambah school year:", err.response ? err.response.data : err)
+    throw err.response ? err.response.data : err;
+  }
 }
 
 export const activeschoolyear = async (id) => {
-    try {
-        const res = await api.post(`/school-years/${id}/activate`)
-        notify("Data Berhasil Diactivkan");
-        return res.data; 
-    } catch (err) {
-        console.error("Gagal mengaktifkan school year:", err.response ? err.response.data : err)
-        throw err.response ? err.response.data : err;
-    }
+  try {
+    const res = await api.post(`/school-years/${id}/activate`)
+    notify("Data Berhasil Diaktifkan");
+    return res.data;
+  } catch (err) {
+    console.error("Gagal mengaktifkan school year:", err.response ? err.response.data : err)
+    throw err.response ? err.response.data : err;
+  }
 }
 
 export const deleteschoolyears = async (id) => {
-    try {
-        const res = await api.delete(`/school-years/${id}`);
-         notify('Data Berhasil Dihapus');
-        return res.data; 
-    } catch (err) {
-        throw err.response ? err.response.data : new Error("Terjadi kesalahan jaringan.");
-    }
+  try {
+    const res = await api.delete(`/school-years/${id}`);
+    notify('Data Berhasil Dihapus');
+    return res.data;
+  } catch (err) {
+    throw err.response ? err.response.data : new Error("Terjadi kesalahan jaringan.");
+  }
 };
 
 
