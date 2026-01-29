8d3d294d9b6e6f7564237440105ddd7ce22599bd
nando <ham280207@gmail.com>
2026-01-30 00:15:37 +0700

    feat: add notif attendance rules

--- src/Core/api/role-operator/attendance-rules/attendanceRules.api.js
+++ src/Core/api/role-operator/attendance-rules/attendanceRules.api.js
@@ -1,11 +1,14 @@
 import api from "../../axiosConfig";
+import { notify } from "../../../hooks/notification/notify";
 
 export const fetchAttendanceRulesAPI = (day) => {
   return api.get(`/attendance-rules/${day}`);
 };
 
-export const saveAttendanceRuleAPI = (selectedDay, payload) => {
-  return api.put(`/attendance-rules/${selectedDay}`, payload);
+export const saveAttendanceRuleAPI = async (selectedDay, payload) => {
+  const response = await api.put(`/attendance-rules/${selectedDay}`, payload);
+  notify("Berhasil menyimpan data!", "success");
+  return response;
 };
 
 export const createAttendanceRuleApi = (payload) => {
