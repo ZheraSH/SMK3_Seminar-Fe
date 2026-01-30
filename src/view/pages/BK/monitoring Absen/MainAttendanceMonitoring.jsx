fa60971a372c21f08bd1c85b86ed21fd7885bb47
afantino02042007-dot <afantino02042007@gmail.com>
2026-01-30 07:55:18 +0700

    refactor: Monitoring role BK

--- src/view/pages/BK/monitoring Absen/MainAttendanceMonitoring.jsx
+++ src/view/pages/BK/monitoring Absen/MainAttendanceMonitoring.jsx
@@ -1,98 +1,199 @@
-import { RefreshCw } from "lucide-react";
-import FilterDropdown from "./components/FilterData";
-import TableAttendanceBk from "./components/TableAttendance";
-import Head from "./components/Head";
-import StatisticsCrad from "./components/StatisticsCard";
-import { Pagination } from "./components/Pagination";
-import { useAttendanceMonitoring } from "../../../../Core/hooks/bk-hooks/AttendanceMonitoring/useAttendance";
+'use client'
 
-export default function MainMonitoringAbsen() {
-    const {
-    students,
-    recap,
-    loading,
-    error,
-    searchQuery,
-    selectedFilter,
-    lastUpdated,
-    currentPage,
-    totalPages,
-    noData,
-    handleSearchChange,
-    handleFilterSelect,
-    handlePageChange,
-    fetchData,
-} = useAttendanceMonitoring();
+import { useEffect, useState } from 'react'
+import {
+  AlertTriangle,
+  ChevronLeft,
+  ChevronRight,
+  CircleCheckBig,
+  ClockAlert,
+  ClipboardCheck
+} from 'lucide-react'
+import Header from '../../../components/elements/header/Header-new'
 
+export default function AttendanceDashboard() {
+  const [data, setData] = useState([])
+  const [summary, setSummary] = useState({
+    hadir: 0,
+    telat: 0,
+    izin: 0,
+    alpha: 0
+  })
+  const [loading, setLoading] = useState(true)
 
-    return (
-        <div className="justify-center mx-5 mb-10">
-            <Head />
-            <StatisticsCrad recap={recap} />
-            <div className="flex justify-between mt-5 gap-1">
-                <div className="relative flex">
-                    <div className="relative flex items-center w-[180px] md:w-80 mr-2 md:mr-4">
-                        <svg className="absolute left-3 w-5 h-5 text-gray-400" fill="none" stroke="currentColor"
-                            viewBox="0 0 24 24">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
-                        <input
-                            type="text"
-                            placeholder="Cari Kelas/Siswa...."
-                            className="p-2 pl-10 border border-gray-300 rounded-full w-[180px] md:w-full focus:outline-none text-[10px] md:text-[16px] focus:ring-2 focus:ring-blue-500"
-                            value={searchQuery}
-                            onChange={handleSearchChange}
-                        />
-                    </div>
-                    <FilterDropdown
-                        students={students}
-                        selected={selectedFilter}
-                        onSelect={handleFilterSelect}
-                    />
-                </div>
-                <button
-                    className="flex items-center w-auto md:w-[128px] h-[37px] px-1 md:px-4 py-2 bg-[#3B82F6] rounded-lg transition text-sm text-white gap-1 md:gap-2 shadow-md"
-                    onClick={() => fetchData({}, 1)}
-                >
-                    <RefreshCw size={16} />
-                    <p className=" text-[8px] md:text-[14px] font-medium"> Sync Data</p>
-                </button>
+  useEffect(() => {
+    const fetchMonitoring = async () => {
+      try {
+        const res = await fetch(
+          'http://127.0.0.1:8000/api/counselor/attendance/monitoring-global'
+        )
+
+        const result = await res.json()
+
+        const students = result?.data?.students || []
+
+        // hitung summary
+        const summaryResult = students.reduce(
+          (acc, cur) => {
+            acc.hadir += cur.hadir
+            acc.izin += cur.izin
+            acc.alpha += cur.alpha
+            return acc
+          },
+          { hadir: 0, telat: 0, izin: 0, alpha: 0 }
+        )
+        console.log(res.data.data)
+        setData(students)
+        setSummary(summaryResult)
+      } catch (err) {
+        console.error('Fetch monitoring-global gagal:', err)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchMonitoring()
+  }, [])
+
+  return (
+    <div className="min-h-screen bg-gray-50 p-6">
+      <Header
+        span="Monitoring Kehadiran Siswa"
+        p="Lihat perkembangan presensi siswa secara real-time"
+        src="/images/particle/particle12.png"
+      />
+
+      <div className="max-w-7xl mx-auto">
+        {/* Stats Cards */}
+        <div className="grid grid-cols-4 gap-4 mb-6">
+          <div className="bg-white rounded-xl p-4 flex justify-between shadow-md">
+            <div className="bg-[#22C55E] w-[4px] h-[52px] rounded-full" />
+            <div>
+              <div className="text-3xl font-medium">{summary.hadir}</div>
+              <div className="text-sm text-gray-600 mt-1">
+                Total Siswa Hadir
+              </div>
             </div>
-
-            <div className="mt-4">
-                <p className="flex justify-end text-[12px] font-light">
-                    Terakhir diperbarui: {lastUpdated 
-                        ? lastUpdated.toLocaleString("id-ID", {
-                            day: "2-digit",
-                            month: "short",
-                            year: "numeric",
-                            hour: "2-digit",
-                            minute: "2-digit",
-                        })
-                        : "–"}
-                </p>
-
-                <div className=" overflow-auto lg:overflow-hidden w-full px-2 md:px-3">
-                    {noData ? (
-                        <p className="text-center text-gray-500 text-lg py-4">
-                            Data tidak ditemukan
-                        </p>
-                    ) : (
-                        <TableAttendanceBk
-                            students={students}
-                            loading={loading}
-                            error={error}
-                        />
-                    )}
-                </div>
-
-                <Pagination
-                    currentPage={currentPage}
-                    totalPages={totalPages}
-                    onPageChange={handlePageChange}
-                />
+            <div className="w-12 h-12 bg-[#DBFFEE] rounded-md flex items-center justify-center">
+              <CircleCheckBig className="text-[#22C55E]" />
             </div>
+          </div>
+
+          <div className="bg-white rounded-xl p-4 flex justify-between shadow-md">
+            <div className="bg-[#F59E0B] w-[4px] h-[52px] rounded-full" />
+            <div>
+              <div className="text-3xl font-medium">{summary.telat}</div>
+              <div className="text-sm text-gray-600 mt-1">
+                Total Siswa Telat
+              </div>
+            </div>
+            <div className="w-12 h-12 bg-[#FEF3C7] rounded-md flex items-center justify-center">
+              <ClockAlert className="text-[#F59E0B]" />
+            </div>
+          </div>
+
+          <div className="bg-white rounded-xl p-4 flex justify-between shadow-md">
+            <div className="bg-[#0EA5E9] w-[4px] h-[52px] rounded-full" />
+            <div>
+              <div className="text-3xl font-medium">{summary.izin}</div>
+              <div className="text-sm text-gray-600 mt-1">
+                Total Siswa Izin
+              </div>
+            </div>
+            <div className="w-12 h-12 bg-[#DBEAFE] rounded-md flex items-center justify-center">
+              <ClipboardCheck className="text-[#0EA5E9]" />
+            </div>
+          </div>
+
+          <div className="bg-white rounded-xl p-4 flex justify-between shadow-md">
+            <div className="bg-[#EF4444] w-[4px] h-[52px] rounded-full" />
+            <div>
+              <div className="text-3xl font-medium">{summary.alpha}</div>
+              <div className="text-sm text-gray-600 mt-1">
+                Total Siswa Alpha
+              </div>
+            </div>
+            <div className="w-12 h-12 bg-[#FEE2E2] rounded-md flex items-center justify-center">
+              <AlertTriangle className="text-[#EF4444]" />
+            </div>
+          </div>
         </div>
-    );
+
+        {/* Table */}
+        <div className="bg-white rounded-lg  overflow-hidden">
+          <table className="w-full">
+            <thead className="bg-blue-500 text-white">
+              <tr className="grid grid-cols-6">
+                <th className="px-6 py-3 text-left">Nama</th>
+                <th className="px-6 py-3 text-left">Kelas</th>
+                <th className="px-6 py-3 text-left">Hadir</th>
+                <th className="px-6 py-3 text-left">Izin</th>
+                <th className="px-6 py-3 text-left">Sakit</th>
+                <th className="px-6 py-3 text-left">Alpha</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {loading ? (
+                <tr>
+                  <td colSpan="6" className="text-center  py-6">
+                    Loading...
+                  </td>
+                </tr>
+              ) : data.length === 0 ? (
+                <tr>
+                  <td colSpan="6" className="text-center  py-6">
+                    Tidak ada data
+                  </td>
+                </tr>
+              ) : (
+                data.map((row, i) => (
+                  <tr
+                    key={i}
+                    className={`grid grid-cols-6 border-b ${
+                      i % 2 === 0 ? 'bg-gray-50' : 'bg-white'
+                    }`}
+                  >
+                    <td className="px-6 py-3">{row.student_name}</td>
+                    <td className="px-6 py-3">{row.classroom}</td>
+                    <td className="px-6 py-3">
+                      <span className="bg-emerald-100 text-emerald-700 px-3 py-1 rounded text-xs">
+                        {row.hadir}
+                      </span>
+                    </td>
+                    <td className="px-6 py-3">
+                      <span className="bg-amber-100 text-amber-700 px-3 py-1 rounded text-xs">
+                        {row.izin}
+                      </span>
+                    </td>
+                    <td className="px-6 py-3">
+                      <span className="bg-blue-100 text-blue-700 px-3 py-1 rounded text-xs">
+                        {row.sakit}
+                      </span>
+                    </td>
+                    <td className="px-6 py-3">
+                      <span className="bg-red-100 text-red-700 px-3 py-1 rounded text-xs">
+                        {row.alpha}
+                      </span>
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        </div>
+
+        {/* Pagination dummy */}
+        <div className="flex justify-center gap-2 mt-6">
+          <button className="p-2">
+            <ChevronLeft />
+          </button>
+          <button className="w-8 h-8 bg-blue-500 text-white rounded">1</button>
+          <button className="p-2">
+            <ChevronRight />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
