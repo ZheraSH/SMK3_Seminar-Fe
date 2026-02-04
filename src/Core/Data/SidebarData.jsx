209bdb6695156cae9c6ee4e2d49500ab77a36342
Dwi Rahmawati <xqsdecii@gmail.com>
2026-02-04 15:59:14 +0700

    refactor: data sidebar

--- src/Core/Data/SidebarData.jsx
+++ src/Core/Data/SidebarData.jsx
@@ -1,18 +1,12 @@
 import {
-    Contact,
     Home,
     UsersRound,
     DoorOpen,
-    IdCardLanyard,
     Calendar1,
-    ChartColumnBig,
     CalendarDays,
-    Clock,
     UserRound,
     CircleCheckBig ,
     FileText,
-    Activity,
-    ClipboardCheck,
     LayoutGrid,
     UserRoundPen,
     Puzzle,
@@ -28,8 +22,9 @@ import {
 
 
 
-//data sidebar  admin
-export const menuItemsOperator = [
+
+export const ROLE_MENUS = {
+  school_operator: [
     { name: "Dashboard", icon: <LayoutGrid size={20} />, path: "/home" },
     { name: "Guru", icon: <UserRoundPen size={20} />, path: "/home/guru" },
     { name: "Siswa", icon: <UsersRound size={20} />, path: "/home/siswa" },
@@ -41,64 +36,34 @@ export const menuItemsOperator = [
     { name: "Pengaturan Jam Absen", icon: <CalendarClock size={20} />, path: "/home/absen-rfid" },
     { name: "Management RFID", icon: <IdCard size={20} />, path: "/home/rfid" },
     { name: "Master Card", icon: <KeyRound size={20} />, path: "/home/mastercard" },
-    
-  ];
-
-
-
-//data sidebar student
-
-export const menuItemSiswa = [
+  ],
+  student: [
     {name: "Dashboard", icon: <LayoutGrid size={20} />, path: "/student-home" },
     {name: "Kelas Saya", icon : <UserRound size={20}/>, path: "/student-home/student-class"},
     {name: "Absensi", icon: <CircleCheckBig size={20} />, path: "/student-home/absen-student"},
     {name: "Jadwal Pelajaran", icon: <CalendarSearch size={20} />, path: "/student-home/student-schedule" },
     {name: "Izin", icon: <FileText size={20}/>, path:"/student-home/student-license" },
-];
-
-
-//data sidebar teacher
-
-export const menuItemTeacher = [
+  ],
+  teacher: [
     {name: "Home", icon:<Home size={20} />, path: "/teacher-home"},
     {name: "Absensi Kelas " , icon: <CircleCheckBig size={20} />, path: "/teacher-home/attendance-teacher"},
     {name: "Jadwal Mengajar",icon: <CalendarDays size={20} />, path: "/teacher-home/teacher-schedule" },
-];
-
-export const menuItemHomeRoom = [
+  ],
+  homeroom_teacher: [
     {name: "Dashboard", icon:<LayoutGrid size={20} />, path: "/homeroom-home/home"},
-    // {name: "Monitoring Kelas", icon:<TrendingUp size={20} />, path: "/homeroom-home/home"},
-    // {name: "Absensi Kelas " , icon: <CircleCheckBig size={20} />, path: "/teacher-home/attendance-teacher"},
-    // {name: "Jadwal Mengajar",icon: <CalendarDays size={20} />, path: "/teacher-home/teacher-schedule" },
     {name: "Rekap Absensi",icon: <FileText size={20} />, path: "/homeroom-home/class-recap" },
-];
+  ],
+  counselor: [
+    {name: "Dashboard", icon:<LayoutGrid size={20}/>, path: "/bk-home"},
+    {name: "Monitoring ", icon:  <TrendingUp size={20} />, path: "/bk-home/monitoring-absen"},
+    {name: "Statistik Global", icon: <ChartNoAxesCombined size={20} />, path: "/bk-home/statistik-global"},
+    {name: " Izin", icon: <FileText  size={20} />, path: "/bk-home/verifikasi-izin"},
+  ]
+};
 
-
-//data sidebar bk 
-export const menuItemBk = [
-  {name: "Dashboard", icon:<LayoutGrid size={20}/>, path: "/bk-home"},
-  {name: "Monitoring ", icon:  <TrendingUp size={20} />, path: "/bk-home/monitoring-absen"},
-  {name: "Statistik Global", icon: <ChartNoAxesCombined size={20} />, path: "/bk-home/statistik-global"},
-  {name: " Izin", icon: <FileText  size={20} />, path: "/bk-home/verifikasi-izin"},
-]
 
 
 
 export const multiRoleCommon = [
   { name: "Dashboard", icon: <LayoutGrid size={20} />, path: "/dashboard" },
-];
-
-export const multiRoleTeacher = [
-  { name: "Absensi Kelas", icon: <CircleCheckBig size={20} />, path: "/dashboard/attendance-teacher" },
-  { name: "Jadwal Mengajar", icon: <CalendarDays size={20} />, path: "/dashboard/teacher-schedule" },
-];
-
-export const multiRoleHomeroom = [
-  { name: "Rekap Absensi", icon: <FileText size={20} />, path: "/dashboard/class-recap" },
-];
-
-export const multiRoleCounselor = [
-  { name: "Monitoring Absen", icon: <TrendingUp size={20} />, path: "/dashboard/monitoring-absen" },
-  { name: "Verifikasi Izin", icon: <FileText size={20} />, path: "/dashboard/verifikasi-izin" },
-  { name: "Statistik Global", icon: <ChartNoAxesCombined size={20} />, path: "/dashboard/statistik-global" },
 ];
