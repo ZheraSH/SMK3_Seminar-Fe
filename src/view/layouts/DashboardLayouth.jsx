ede72fd1a0b3ad9a516fa59faf675f6e1c813dde
Dwi Rahmawati <xqsdecii@gmail.com>
2026-02-04 14:05:07 +0700

    Refactor: DashboardLayout

--- src/view/layouts/DashboardLayouth.jsx
+++ src/view/layouts/DashboardLayouth.jsx
@@ -1,19 +1,46 @@
 import { Link, Outlet, useLocation } from "react-router-dom";
-import { menuItemsOperator } from "@data/SidebarData";
-import { useRef, useState, useEffect } from "react";
+import { menuItemsOperator, menuItemTeacher, menuItemHomeRoom, menuItemBk, menuItemSiswa } from "@data/SidebarData";
+import { useRef, useState, useEffect, useMemo } from "react";
 import MainDashboard from "../components/elements/MainDashboard";
 import { ChevronDown } from "lucide-react";
 import { Notification } from "../../Core/hooks/notification/Notification";
 import LoginSuccessPopup from "../components/elements/succeslogin/LoginSuccessPopup";
-import {  SidebarStaggerContainer,  SidebarItemReveal,  CharReveal } from "../components/animate/animate";
+import useProfile from "../../Core/hooks/operator-hooks/profile/useProfileOperator";
 
 export const DashboardLayouth = () => {
   const location = useLocation();
   const scrollRef = useRef(null);
-
+  
+  const { schoolInfo } = useProfile();
   const [showScrollButton, setShowScrollButton] = useState(false);
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [user, setUser] = useState({ roles: [] });
   const [showLoginPopup, setShowLoginPopup] = useState(false);
+
+  useEffect(() => {
+    const stored = localStorage.getItem("userData");
+    if (stored) {
+      const parsedData = JSON.parse(stored);
+      setUser({
+        ...parsedData,
+        roles: Array.isArray(parsedData.roles) ? parsedData.roles : [parsedData.role || parsedData.roles]
+      });
+    }
+  }, []);
+
+  
+  const currentMenuItems = useMemo(() => {
+    const roleMapping = [
+      { role: "teacher", menu: menuItemTeacher },
+      { role: "homeroom_teacher", menu: menuItemHomeRoom },
+      { role: "counselor", menu: menuItemBk },
+      { role: "student", menu: menuItemSiswa },
+      { role: "school_operator", menu: menuItemsOperator }
+    ];
+
+    const match = roleMapping.find(m => user.roles.includes(m.role));
+    return match ? match.menu : [];
+  }, [user.roles]);
 
   useEffect(() => {
     const handleResize = () => {
@@ -67,44 +94,42 @@ export const DashboardLayouth = () => {
         <div className={`fixed top-0 left-0 h-full z-40 transform transition-all duration-300 ${sidebarOpen ? "w-[250px] translate-x-0" : "w-[80px] -translate-x-full lg:translate-x-0"}`}>
           <div className={`h-full bg-white ${sidebarOpen ? "" : "overflow-hidden"}`}>
             <div className={`flex justify-center items-center gap-3 py-6 ${sidebarOpen ? "px-10" : ""}`}>
-              <img src="../images/SMKNLOGO1.png" className="w-10 h-10 " alt="SMKN Logo"/>
+              <img src={schoolInfo?.logo} className="w-10 h-10 " alt="SMKN Logo"/>
               {sidebarOpen && (
-                <h1 className="flex-wrap text-[16px] font-semibold"> SMK NEGERI 3 Pamekasan</h1>
+                <h1 className="flex-wrap text-[16px] font-semibold"> {schoolInfo?.name || "-"} </h1>
               )}
             </div>
 
             <div ref={scrollRef} className={`overflow-y-auto ${ sidebarOpen ? "h-[calc(100vh-100px)] [&::-webkit-scrollbar]:hidden" : "h-[calc(100vh-80px)]"}`}>
               <div>
-                {menuItemsOperator.map((item, index) => {
-                let active = location.pathname === item.path;
+                {currentMenuItems.map((item, index) => {
+                  let active = location.pathname === item.path;
 
-                if (item.path === "/home/class") {
-                  active =
-                    [ "/home/class"].includes(location.pathname) ||
-                    location.pathname.startsWith("/home/classStudents");
-                }
+                  if (item.path === "/home/class") {
+                    active =
+                      [ "/home/class"].includes(location.pathname) ||
+                      location.pathname.startsWith("/home/classStudents");
+                  }
 
-                return (
-                  <div key={index}>
-                    <Link to={item.path} className={`relative flex items-center mb-1.5 hover:bg-[#E5F0FF] ${sidebarOpen ? "gap-3 p-2 pl-5" : "justify-center p-3"} ${active ? "bg-[#3B82F61F] text-[#3B82F6]" : ""}`} title={!sidebarOpen ? item.name : ""}>
-                    {active && sidebarOpen && (
-                      <div className="absolute left-0 top-0 h-full w-[4px] bg-[#3B82F6] rounded-r-lg" />
-                    )}
-                    <div className={`${ active && !sidebarOpen ? "text-[#3B82F6]" : "" }`}>
-                      {item.icon}
+                  return (
+                    <div key={index}>
+                      <Link to={item.path} className={`relative flex items-center mb-1.5 hover:bg-[#E5F0FF] ${sidebarOpen ? "gap-3 p-2 pl-5" : "justify-center p-3"} ${active ? "bg-[#3B82F61F] text-[#3B82F6]" : ""}`} title={!sidebarOpen ? item.name : ""}>
+                        {active && sidebarOpen && (
+                          <div className="absolute left-0 top-0 h-full w-[4px] bg-[#3B82F6] rounded-r-lg" />
+                        )}
+                        <div className={`${ active && !sidebarOpen ? "text-[#3B82F6]" : "" }`}>
+                          {item.icon}
+                        </div>
+                        {sidebarOpen && (
+                          <span className="text-[14px] font-semibold whitespace-nowrap">
+                            {item.name}
+                          </span>
+                        )}
+                      </Link>
                     </div>
-                    {sidebarOpen && (
-                      <span className="text-[14px] font-semibold whitespace-nowrap">
-                        {item.name}
-                      </span>
-                    )}
-                  </Link>
-                  </div>
-                  
-                );
-              })}
+                  );
+                })}
               </div>
-              
 
               {showScrollButton && sidebarOpen && (
                 <button onClick={scrollToBottom} className="absolute bottom-4 right-4 bg-white rounded-full p-2 shadow-lg">
