8ba4f9302f0134b54a23f0488e149174d1498724
nando <ham280207@gmail.com>
2026-04-18 20:48:24 +0700

    refactor: file PascalCase to kebab-case

--- src/view/pages/student/student-class/main-class.jsx
+++ src/view/pages/student/student-class/main-class.jsx
@@ -1,6 +1,6 @@
 import { Users,DoorOpen } from "lucide-react";
 import CardStudent from "./components/card-student";
-import { Pagination } from "./components/pagination";
+import { Pagination } from "./components/student-pagination";
 import { useClassStudent } from "@/core/hooks/role-student/classroom/use-classroom";
 import LoadingData from "@elements/loading-data/loading";
 
