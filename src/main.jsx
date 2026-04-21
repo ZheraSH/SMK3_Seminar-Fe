5e4f99e9ce4e3d71e50b72e40de3aaf0416eda72
Farah Amalia <140934377+farahamaliaa@users.noreply.github.com>
2026-04-21 16:52:23 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
 import "./index.css";
-import { router } from "./core/router/index";
+import { router } from "@core/router/Index";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
